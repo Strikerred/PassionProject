a947839532ba0bb823c0db070550ab707f155411
Fausto Gomez <gomez_fausto@hotmail.com>
2020-03-18 01:51:20 -0700

    Add database Context with users and roles tables

---
+++ BirthDayCards/Models/BirthDayCard_dbContext.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace BirthDayCards.Models
+{
+    public partial class BirthDayCard_dbContext : DbContext
+    {
+        public BirthDayCard_dbContext()
+        {
+        }
+
+        public BirthDayCard_dbContext(DbContextOptions<BirthDayCard_dbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Roles> Roles { get; set; }
+        public virtual DbSet<Users> Users { get; set; }
+
+        // Unable to generate entity type for table 'dbo.Person'. Please see the warning messages.
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Name=ConnectionStrings:DefaultConnection");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.6-servicing-10079");
+
+            modelBuilder.Entity<Roles>(entity =>
+            {
+                entity.HasKey(e => e.RoleId)
+                    .HasName("PK__Roles__8AFACE1A6D7D1B97");
+
+                entity.Property(e => e.RoleName)
+                    .IsRequired()
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Users>(entity =>
+            {
+                entity.HasKey(e => e.UserName)
+                    .HasName("PK__Users__C9F28457AFADA90D");
+
+                entity.Property(e => e.UserName)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .ValueGeneratedNever();
+
+                entity.HasOne(d => d.Role)
+                    .WithMany(p => p.Users)
+                    .HasForeignKey(d => d.RoleId)
+                    .HasConstraintName("FK__Users__RoleId__75A278F5");
+            });
+        }
+    }
+}
---
+++ BirthDayCards/Models/Users.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BirthDayCards.Models
+{
+    public partial class Users
+    {
+        public string UserName { get; set; }
+        public int? RoleId { get; set; }
+
+        public virtual Roles Role { get; set; }
+    }
+}
